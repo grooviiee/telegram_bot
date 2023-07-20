9e15db2e53dad7bd946d4a251c5cce0de50fa307
grooviiee2 <86159019+grooviiee@users.noreply.github.com>
2023-07-21 01:02:42 +0900

    Update init.py

--- init.py
+++ init.py
@@ -12,3 +12,20 @@ msg = 'new is start yes'
 
 bot = telegram.Bot(token=MY_TOKEN)
 asyncio.run(send_text(bot, msg))
+
+
+import asyncio
+import telegram
+import json
+import requests
+
+MY_TOKEN = '6381769851:AAE_7t57ut7jh0-pHZWTZz2f0T96BulTWmY'
+CHAT_ID = '798391124'
+
+
+def send_message(message):
+  url=f'https://api.telegram.org/bot{MY_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}'
+  response=json.loads(requests.get(url).text)
+  print(response)
+
+send_message('반가워요')
